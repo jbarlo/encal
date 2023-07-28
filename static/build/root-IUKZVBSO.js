b262a2afdf523a09cd388ac2698ad200a54be543
James Barlow <james-b-git@outlook.com>
2023-07-27 17:59:51 -0700

    Add static folder

---
+++ static/build/root-IUKZVBSO.js
@@ -0,0 +1,15 @@
+import{a as i,b as o,c as r,d,e as l,g as f}from"/build/_shared/chunk-NLTB2QEE.js";import{d as n,f as s}from"/build/_shared/chunk-SF3EXJND.js";var t;var e=n(s()),p=()=>[...t?[{rel:"stylesheet",href:t}]:[]];function a(){return(0,e.jsxs)("html",{lang:"en",children:[(0,e.jsxs)("head",{children:[(0,e.jsx)("meta",{charSet:"utf-8"}),(0,e.jsx)("meta",{name:"viewport",content:"width=device-width,initial-scale=1"}),(0,e.jsx)(r,{}),(0,e.jsx)(o,{})]}),(0,e.jsxs)("body",{children:[(0,e.jsx)(i,{}),(0,e.jsx)(f,{}),(0,e.jsx)(d,{}),(0,e.jsx)(l,{})]})]})}export{a as default,p as links};
+/*! Bundled license information:
+
+@remix-run/css-bundle/dist/esm/index.js:
+  (**
+   * @remix-run/css-bundle v1.19.1
+   *
+   * Copyright (c) Remix Software Inc.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE.md file in the root directory of this source tree.
+   *
+   * @license MIT
+   *)
+*/
